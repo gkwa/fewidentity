40cebc07807c13224a5feea0d5b7b0554100ebe1
Taylor Monacelli <taylormonacelli@gmail.com>
2024-07-09 14:49:36 -0700

    show source line numbers in logs

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -3,6 +3,8 @@ package logger
 import (
 	"io"
 	"os"
+	"path/filepath"
+	"strconv"
 	"time"
 
 	"github.com/fatih/color"
@@ -16,8 +18,16 @@ import (
 func NewConsoleLogger(verbose bool, jsonFormat bool) logr.Logger {
 	var zlog zerolog.Logger
 
+	zerolog.CallerMarshalFunc = func(pc uintptr, file string, line int) string {
+		relPath, err := filepath.Rel(".", file)
+		if err != nil {
+			relPath = file
+		}
+		return relPath + ":" + strconv.Itoa(line)
+	}
+
 	if jsonFormat {
-		zlog = zerolog.New(os.Stderr).With().Timestamp().Logger()
+		zlog = zerolog.New(os.Stderr).With().Timestamp().Caller().Logger()
 	} else {
 		color.NoColor = !verbose
 		consoleWriter := zerolog.ConsoleWriter{
@@ -30,7 +40,7 @@ func NewConsoleLogger(verbose bool, jsonFormat bool) logr.Logger {
 			consoleWriter.PartsExclude = []string{zerolog.TimestampFieldName}
 		}
 
-		zlog = zerolog.New(consoleWriter).With().Timestamp().Logger()
+		zlog = zerolog.New(consoleWriter).With().Timestamp().Caller().Logger()
 	}
 
 	if verbose {
