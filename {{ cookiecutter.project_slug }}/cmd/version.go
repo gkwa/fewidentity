d27c5980372c17c246118df7e4ce5d8ea410fcc7
Taylor Monacelli <taylormonacelli@gmail.com>
2024-07-25 15:35:06 -0700

    make version prettier

--- {{ cookiecutter.project_slug }}/cmd/version.go
+++ {{ cookiecutter.project_slug }}/cmd/version.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"fmt"
+	
 	"github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}/version"
 	"github.com/spf13/cobra"
 )
@@ -10,9 +12,8 @@ var versionCmd = &cobra.Command{
 	Short: "Print the version number of {{ cookiecutter.project_slug }}",
 	Long:  `All software has versions. This is {{ cookiecutter.project_slug }}'s`,
 	Run: func(cmd *cobra.Command, args []string) {
-		logger := LoggerFrom(cmd.Context())
 		buildInfo := version.GetBuildInfo()
-		logger.Info("Version info", "version", buildInfo)
+		fmt.Println("Version info", "version", buildInfo)
 	},
 }
 
